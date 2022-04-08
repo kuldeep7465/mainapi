fa585867652b0d84ecfcda765cb1b3c5235a85b5
kuldeep7465 <91813803+kuldeep7465@users.noreply.github.com>
2022-04-08 14:32:24 +0530

    Add files via upload

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webapikuldeep.connect;
+
+namespace webapikuldeep.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+
+        [HttpGet]
+        [Route("emp/getemployee")]
+        public List<my_table> Kuldeep()
+        {
+            kuldeepEntities obj = new kuldeepEntities();
+            var res = obj.my_table.ToList();
+            return res;
+
+        }
+        [HttpPost]
+        [Route("emp/saveemployee")]
+        public HttpResponseMessage SaveEmp(my_table Obj)
+        {
+
+            kuldeepEntities Dbcon = new kuldeepEntities();
+
+            my_table tblEmp = new my_table();
+
+            if (Obj.id == 0)
+            {
+                Dbcon.my_table.Add(Obj);
+                Dbcon.SaveChanges();
+
+            }
+
+            else
+            {
+                Dbcon.Entry(Obj).State = System.Data.Entity.EntityState.Modified;
+                Dbcon.SaveChanges();
+
+            }
+
+            HttpResponseMessage res = new HttpResponseMessage(HttpStatusCode.OK);
+
+            return res;
+        }
+    }
+}
